59256ae59845ea18df6873a78076877b92eaae00
crazymoose77756 <52980616+crazymoose77756@users.noreply.github.com>
2024-08-24 10:21:19 -0400

    Initial commit

---
+++ src/main/java/com/example/addon/commands/CommandExample.java
@@ -0,0 +1,34 @@
+package com.example.addon.commands;
+
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import meteordevelopment.meteorclient.commands.Command;
+import net.minecraft.command.CommandSource;
+
+import static com.mojang.brigadier.Command.SINGLE_SUCCESS;
+
+/**
+ * The Meteor Client command API uses the <a href="https://github.com/Mojang/brigadier">same command system as Minecraft does</a>.
+ */
+public class CommandExample extends Command {
+    /**
+     * The {@code name} parameter should be in kebab-case.
+     */
+    public CommandExample() {
+        super("example", "Sends a message.");
+    }
+
+    @Override
+    public void build(LiteralArgumentBuilder<CommandSource> builder) {
+        builder.executes(context -> {
+            info("hi");
+            return SINGLE_SUCCESS;
+        });
+
+        builder.then(literal("name").then(argument("nameArgument", StringArgumentType.word()).executes(context -> {
+            String argument = StringArgumentType.getString(context, "nameArgument");
+            info("hi, " + argument);
+            return SINGLE_SUCCESS;
+        })));
+    }
+}
